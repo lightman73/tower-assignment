f51d27bba66aa18d040a702ead276c2b8c0928a4
Francesco Marini <franz.marini@gmail.com>
2021-04-18 10:48:51 +0200

    chore: prepare the project structure
    Remove unused default VC, add groups for VCs, models and components

---
+++ tower-assignment/ViewControllers/MainVC.swift
@@ -0,0 +1,36 @@
+//
+//  MainVC.swift
+//  tower-assignment
+//
+//  Created by Francesco Marini on 18/04/21.
+//
+
+import Foundation
+import UIKit
+
+class MainVC: UIViewController {
+    // MARK: - Properties
+    
+    
+    // MARK: - Outlets
+    
+    
+    // MARK: - Overrided functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    // MARK: - UI functions
+    
+    
+    // MARK: - Custom functions
+    
+    
+    // MARK: - Actions
+    
+    
+    // MARK: - Navigation
+    
+}
