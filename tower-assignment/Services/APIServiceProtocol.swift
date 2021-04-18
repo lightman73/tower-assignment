3aa352059f3e1ad822705be256ffd14d8da97121
Francesco Marini <franz.marini@gmail.com>
2021-04-18 18:17:35 +0200

    feat: add API Service protocol

---
+++ tower-assignment/Services/APIServiceProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  APIServiceProtocol.swift
+//  tower-assignment
+//
+//  Created by Francesco Marini on 18/04/21.
+//
+
+import Foundation
+
+
+public enum APIServiceError: LocalizedError {
+    case serverError(Int?, String?, Error)          // http status code, error description, error from server
+    case dataConversionError
+    case missingData
+}
+
+
+public protocol APIService: class {
+    func getCommitsList(handler: @escaping (Result<[Commit], APIServiceError>) -> Void)
+    func getChangesetFrom(commit: String, handler: @escaping (Result<[ChangesetEntry], APIServiceError>) -> Void)
+    func getDiffFor(changeset: String, handler: @escaping (Result<String?, APIServiceError>) -> Void)
+}
