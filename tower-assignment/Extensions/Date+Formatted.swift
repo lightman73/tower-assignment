f687dc1b3395e440c3ae0c96d58e78fa6eb83924
Francesco Marini <franz.marini@gmail.com>
2021-04-19 00:54:43 +0200

    fix: correct date <-> string conversions

--- tower-assignment/Extensions/Date+Formatted.swift
+++ tower-assignment/Extensions/Date+Formatted.swift
@@ -10,13 +10,19 @@ import Foundation
 extension Date {
     var formattedDate: String {
         let dateFormatter =  DateFormatter()
-        dateFormatter.dateStyle = .medium
+        dateFormatter.dateStyle = .full
         return dateFormatter.string(from: self)
     }
-    
+
     var formattedShortDate: String {
         let dateFormatter =  DateFormatter()
         dateFormatter.dateStyle = .short
+        return dateFormatter.string(from: self)
+    }
+    
+    var formattedDateTime: String {
+        let dateFormatter =  DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
         return dateFormatter.string(from: self)
     }
 }
--- tower-assignment/Extensions/String+Date.swift
+++ tower-assignment/Extensions/String+Date.swift
@@ -9,10 +9,8 @@ import Foundation
 
 extension String {
     var iso8601StringToDate: Date? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        dateFormatter.timeZone = TimeZone.current
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [.withInternetDateTime]
         return dateFormatter.date(from: self)
     }
 }
