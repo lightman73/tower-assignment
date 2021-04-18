ee8e2768c836bce6b2aa9987888b171b54c99697
Francesco Marini <franz.marini@gmail.com>
2021-04-19 01:05:28 +0200

    feat: add some tests

--- tower-assignmentTests/tower_assignmentTests.swift
+++ tower-assignmentTests/tower_assignmentTests.swift
@@ -30,4 +30,52 @@ class tower_assignmentTests: XCTestCase {
         }
     }
 
+    func testISO8601Extensions() {
+        let stringDate = "2021-04-18T18:27:32Z"
+        let convertedDate = stringDate.iso8601StringToDate
+        
+        XCTAssertFalse(convertedDate == nil)
+        
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [.withInternetDateTime]
+        
+        let dateBackToString = dateFormatter.string(from: convertedDate ?? Date())
+        
+        XCTAssert(dateBackToString == stringDate)
+    }
+    
+    func testCommitInitializesCorrectly() {
+        let commit = Commit(hash: "ec0f93a", authorName: "Jon Snow", authorEmail: "jon.snow@stark.com", subject: "Just a test commit", date: "2021-04-18T18:27:32Z".iso8601StringToDate ?? Date(timeIntervalSince1970: 0))
+        
+        XCTAssert(commit.hash == "ec0f93a")
+        XCTAssert(commit.authorName == "Jon Snow")
+        XCTAssert(commit.authorEmail == "jon.snow@stark.com")
+        XCTAssert(commit.subject == "Just a test commit")
+        XCTAssertFalse(commit.date == Date(timeIntervalSince1970: 0))
+    }
+    
+    func testChangesetEntryInitializesCorrectly() {
+        let changesetEntry = ChangesetEntry(status: .added, filename: "test")
+        
+        XCTAssert(changesetEntry.status == .added)
+        XCTAssert(changesetEntry.filename == "test")
+    }
+    
+    func testMockAPIService() {
+        let mockAPIService = MockAPIService()
+        
+        let testCommmit = Commit(hash: "ec0f93a", authorName: "Jon Snow", authorEmail: "jon.snow@stark.com", subject: "Just a test commit", date: "2021-04-18T18:27:32Z".iso8601StringToDate ?? Date(timeIntervalSince1970: 0))
+        
+        mockAPIService.getCommitsList() { result in
+            switch result {
+            case .success(let commits):
+                XCTAssertFalse(commits.count == 0)
+                XCTAssert(commits[0] == testCommmit)
+            case .failure(_):
+                break
+            }
+        }
+        
+        // The same could be done for the other mocked calls
+    }
 }
